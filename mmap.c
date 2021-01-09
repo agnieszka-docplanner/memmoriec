f5a5996579df9815510fbef9500d0717096ad82d
Agnieszka <agnieszka.pietruczuk@docplanner.com>
2021-01-09 23:53:18 +0100

    mmap

--- mmap.c
+++ mmap.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mman.h>
+
+#define PAGESIZE 4096
 
 int main() {
     for(int i=0; i<5; i++) {
@@ -14,9 +17,15 @@ int main() {
 
     //seg fault
     int *ptr = (int *)third+1;
-    *ptr = 0xDEAD;
+    //*ptr = 0xDEAD;
 
     printf("First: %p\n", first);
     printf("Second: %p\n", second);
     printf("Third: %p\n", third);
+
+    u_int8_t* map_first = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    u_int8_t* map_second = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+    printf("First: %p\n", map_first);
+    printf("Second: %p\n", map_second);
 }
